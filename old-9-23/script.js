08338854def8e9e088e6c2d89a45bc0c6541acad
Trevor Tomesh <slugzzz@gmail.com>
2026-09-23 13:51:56 -0500

    more js fun

---
+++ old-9-23/script.js
@@ -0,0 +1,52 @@
+// This is a single line comment in javascript
+
+/* Multi
+    line
+    comment!
+*/
+
+// function greet(name){
+//     console.log("Hello, " + name + "!");
+// }
+
+// greet("Trevor");
+// output to the console
+// console.log("Hello, world!");
+
+// var age = 30; // numbers!
+// var name = "John" // strings!
+// var isStudent = true; // boolean!
+// console.log(name + " " + age);
+// var x = 5;
+// var y = 3;
+// var sum = x+y;
+// console.log(sum);
+
+// var age = 24;
+
+// if(age < 18){
+//     console.log("You are a minor!");
+// } else if (age >= 18 && age < 65){
+//     console.log("You are an adult!");
+// }else{
+//     console.log("Okay.... boomer...!");
+// }
+
+// for (var i = 0; i < 5; i++){
+//     console.log("Iteration " + i);
+// }
+
+var fruits = ["apple", "banana", "cherry"];
+console.log(fruits[0]);
+
+for(const fruit of fruits){
+    console.log(fruit);
+}
+
+var person = {
+    firstName: "Oscar",
+    lastName: "Mayer",
+    age: 30
+};
+
+console.log(person.firstName);
